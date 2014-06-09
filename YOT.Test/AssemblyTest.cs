404f69d32b2f7704dc27231663688c225f60e143
Rayane Rahmani <rayane.rahmani@hotmail.fr>
2014-06-09 14:24:39 +0200

    Initialisation du projet YOT

---
+++ YOT.Test/AssemblyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace YOT.Test {
+    [TestFixture]
+    public class AssemblyTest {
+
+        private AssemblyLoader assembly = new AssemblyLoader();
+
+        [Test]
+        public void Should_Load_Assembly() {
+            Assert.IsTrue(assembly.loadAssembly("YOT.dll"));
+            Assert.IsFalse(assembly.loadAssembly("YOTFAIL.dll"));
+        }
+
+        /*[Test]
+        public void */
+    }
+}
---
+++ YOT/AssemblyLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YOT
+{
+    public class AssemblyLoader
+    {
+        private Assembly assembly;
+
+        /// <summary>
+        /// Charge la dll passé en paramètre
+        /// </summary>
+        /// <param name="pathAssembly">Chemin de la DLL</param>
+        /// <returns></returns>
+        public bool loadAssembly(string pathAssembly) {
+            try {
+                if (string.IsNullOrEmpty(pathAssembly)) {
+                    Console.WriteLine("Please enter a library path.");
+                    return false;
+                }
+                if (!File.Exists(pathAssembly)) {
+                    Console.WriteLine("This file doesn't exist. Please check your path.");
+                    return false;
+                }
+
+                assembly = Assembly.LoadFrom(pathAssembly);
+                return true;
+            }
+            catch (Exception exp) {
+                Console.WriteLine("An error occured while loading assembly. \n Message : {0}", exp.Message);
+                return false;
+            }
+        }
+
+    }
+}
